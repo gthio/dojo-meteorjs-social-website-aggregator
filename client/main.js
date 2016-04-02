c64af6ffed13fb0c87aa4f0458fa181e2410bdef
gthio <gunawanthio@outlook.com>
2016-04-02 14:47:15 +0800

    Implement add website

--- client/main.js
+++ client/main.js
@@ -25,3 +25,42 @@ Template.websites.helpers({
 	  return Websites.find();
   }
 });
+
+Template.websites.events({
+  
+  'click .js-show-website-form': function (event) {
+    
+    $("#website_add_form").modal('show');
+  }
+});
+
+Template.website_add_form.events({
+  
+  'submit .js-add-website': function(event){
+    
+    var title = event.target
+      .website_title.value;
+    
+    var description = event.target
+      .website_description.value;
+      
+    var url = event.target
+      .website_url.value;
+    
+    if (Meteor.user()) {
+      Websites.insert({
+        
+        title: title,
+        description: description,
+        url: url,
+        createdOn: new Date(),
+        createdBy: Meteor.user()._id
+      });
+    }
+    
+    $("#website_add_form").modal('hide');
+    
+    return false;
+  }
+  
+});
