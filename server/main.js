992375e3ac51c24711e9a20d27bc6fc0e4bba9eb
gthio <gunawanthio@outlook.com>
2016-04-16 05:50:15 +0800

    add anonyfox:scrape package, and use it to collect website's meta data

--- server/main.js
+++ server/main.js
@@ -87,58 +87,16 @@ Meteor.methods({
           
           return null;
         }
-        else{
-
-          var titleTagStart = '<title';
-          var titleTagEnd = '</title>';
-          
-          var descriptionTagStart = 'description" content="';
-          var descriptionTagEnd1 = '">';
-          var descriptionTagEnd2 = '" />';
-          var descriptionTagEnd3 = '"/>';
-                    
-          var keywordTagStart = '<meta name="keywords" content="';          
-          var keyWordTagEnd = '" />';
-          
-          var titleStart = response.content.toLowerCase().indexOf(titleTagStart),            
-            titleStartTo = response.content.toLowerCase().indexOf('>', titleStart),                      
-            titleEnd = response.content.toLowerCase().indexOf(titleTagEnd),
-            
-            titleLength = titleStartTo - titleStart + 1,
-            
-            titleText = response.content.substring(titleStart + titleLength, titleEnd);
-          
-          var descriptionStart = response.content.toLowerCase().indexOf(descriptionTagStart, 1),
-            descriptionEnd1 = response.content.toLowerCase().indexOf(descriptionTagEnd1, descriptionStart),
-            descriptionEnd2 = response.content.toLowerCase().indexOf(descriptionTagEnd2, descriptionStart),
-            descriptionEnd3 = response.content.toLowerCase().indexOf(descriptionTagEnd3, descriptionStart),
-            descriptionEnd = (descriptionEnd2 > 0 && (descriptionEnd2 < descriptionEnd1)) ? descriptionEnd2 : descriptionEnd1,
-            descriptionEnd = (descriptionEnd3 > 0 && (descriptionEnd3 < descriptionEnd)) ? descriptionEnd3 : descriptionEnd,
-            descriptionText = response.content.substring(descriptionStart + descriptionTagStart.length, descriptionEnd);
-          
-            if (descriptionStart == -1){
-              descriptionText = "";
-            } 
-          
-          var keywordStart = response.content.toLowerCase().indexOf(keywordTagStart),
-            keywordEnd = response.content.toLowerCase().indexOf(keyWordTagEnd, keywordStart),
-            keywordText = response.content.substring(keywordStart + keywordTagStart.length, keywordEnd);
+        else{         
+          websiteData = Scrape.website(url);
                   
-          console.log(descriptionStart);
-          console.log(descriptionEnd1);
-          console.log(descriptionEnd2);
-          console.log(descriptionEnd3);
-          console.log(titleText);
-          console.log("xxxx");
-          console.log(descriptionText)
-         
           Websites.insert({
-            title: titleText.trim(),
-                  description: descriptionText.trim(),
-                  url: url,
-                  createdOn: new Date(),
-                  createdBy: userId
-                });   
+            title: websiteData.title,
+            description: websiteData.description,
+            url: websiteData.domain,
+            createdOn: new Date(),
+            createdBy: userId
+          });   
         }
       });    
   },     
